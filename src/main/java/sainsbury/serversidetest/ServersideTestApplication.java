76e6478b245395c8fb2767c40b97d17f7f31f569
Grzegorz Ziemski <ziemski@gmail.com>
2020-03-02 21:51:45 +0000

    New Spring Boot project based on https://start.spring.io/

---
+++ src/main/java/sainsbury/serversidetest/ServersideTestApplication.java
@@ -0,0 +1,13 @@
+package sainsbury.serversidetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServersideTestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ServersideTestApplication.class, args);
+  }
+
+}
---
+++ src/test/java/sainsbury/serversidetest/ServersideTestApplicationTests.java
@@ -0,0 +1,13 @@
+package sainsbury.serversidetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServersideTestApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
